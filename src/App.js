c549e16cdab61859a2f3c7dc9a8b7b58f766566d
Virgile BERRIER <virgile.berrier@gmail.com>
2022-07-12 11:01:07 +0200

    [ADD] Initial commit with blank API

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+class Tweet {
+  state = {};
+  render() {
+
+  }
+}
+
+function App() {
+  console.log("-> App()");
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
